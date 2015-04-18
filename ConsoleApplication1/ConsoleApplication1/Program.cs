8183b3ca7ddff3e4daff56e6a9d33bd6ee2836e4
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-18 17:50:30 -0600

    Functions created

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,18 +11,19 @@ namespace ConsoleApplication1
         static int playerHP = 100;
         static int dragonHP = 200;
         static Random rng = new Random();
-        static string userChoice = Console.ReadLine();
+        static string userChoice = string.Empty;
 
         static void Main(string[] args)
         {
             Console.WriteLine("I hope you are ready for an adventure. You have just learned that you come from a long lineage of Dragon Slayers. To prove your value you must fight a dragon.");
-            Console.WriteLine("\nYou will have three options when fighting this majestic beast. \n1. Use your sword. It will cause 20-35 damage, but will only hit 70% of the time \n2. You can use your magic and it will always hit the dragon, but will only cause damage between 10-15. \n3. Your last option is to heal. This will improve your health anywhere \nfrom 10-20 hp, but you can still be hit by the dragon.");
+            Console.WriteLine("\nYou will have three options when fighting this majestic beast. \n1. Use your sword. It will cause 20-35 damage, but will only hit 70% of the time \n2. You can use your magic and it will always hit the dragon, but will only cause damage between 10-15. \n\n3. Your last option is to heal. This will improve your health anywhere \nfrom 10-20 hp, but you can still be hit by the dragon.");
 
-            while (playerHP >0 && dragonHP >0)
+            while (playerHP > 0 && dragonHP > 0)
             {
-                Console.WriteLine("I hope you are ready for an adventure. You have just learned that you come from a long lineage of Dragon Slayers. To prove your value you must fight a dragon.");
-                Console.WriteLine("\nYou will have three options when fighting this majestic beast. \n1. Use your sword. It will cause 20-35 damage, but will only hit 70% of the time \n2. You can use your magic and it will always hit the dragon, but will only cause damage between 10-15. \n3. Your last option is to heal. This will improve your health anywhere \nfrom 10-20 hp, but you can still be hit by the dragon.");
-
+               
+                UserOptions();
+                DragonAttack();
+                WinOrLose();
             }
 
             Console.ReadKey();
