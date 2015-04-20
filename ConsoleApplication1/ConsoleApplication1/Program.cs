0bfc1eb9df8c5f5143e4a26408e654c77324f5c7
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-20 14:27:44 -0600

    Added Sleep and Clear capabilities

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -16,19 +16,19 @@ namespace ConsoleApplication1
 
         static void Main(string[] args)
         {
+            //name of game and prompt of the info of what options you have when fighting the dragon
+            Console.WriteLine(@"  ___                         ___ _                     ___ ___ 
+ |   \ _ _ __ _ __ _ ___ _ _ / __| |__ _ _  _ ___ _ _  / _ \ __|
+ | |) | '_/ _` / _` / _ \ ' \\__ \ / _` | || / -_) '_| \_, /__ \
+ |___/|_| \__,_\__, \___/_||_|___/_\__,_|\_, \___|_|    /_/|___/
+               |___/                     |__/                   
 
-            Console.WriteLine(@" (                                   (                                              
- )\ )                                )\ )  (                             )  (  (    
-(()/(   (       )  (  (             (()/(  )\    )  (       (   (     ( /(  )\))(   
- /(_))  )(   ( /(  )\))(  (    (     /(_))((_)( /(  )\ )   ))\  )(    )\())((_)()\  
-(_))_  (()\  )(_))((_))\  )\   )\ ) (_))   _  )(_))(()/(  /((_)(()\  ((_)\  (()((_) 
- |   \  ((_)((_)_  (()(_)((_) _(_/( / __| | |((_)_  )(_))(_))   ((_) / _(_)  | __|  
- | |) || '_|/ _` |/ _` |/ _ \| ' \))\__ \ | |/ _` || || |/ -_) | '_| \_, /   |__ \  
- |___/ |_|  \__,_|\__, |\___/|_||_| |___/ |_|\__,_| \_, |\___| |_|    /_/    |___/  
-                  |___/                             |__/                            ");
-
+");
+            
             Console.WriteLine("I hope you are ready for an adventure. You have just learned that you come from a long lineage of Dragon Slayers. To prove your value you must fight a dragon.");
             Console.WriteLine("\nYou will have three options when fighting this majestic beast. \n1. Use your sword. It will cause 20-35 damage, but will only hit 70% of the time \n2. You can use your magic and it will always hit the dragon, but will only cause damage between 10-15. \n\n3. Your last option is to heal. This will improve your health anywhere \nfrom 10-20 hp, but you can still be hit by the dragon.");
+
+
 
             while (playerHP > 0 && dragonHP > 0)
             {
@@ -58,6 +58,8 @@ namespace ConsoleApplication1
                         dragonHP = dragonHP - hpRange;
                         Console.WriteLine("You hit the dragon for {0} hp", (200 - dragonHP));
                         Console.WriteLine("The dragon now has {0} hp.", dragonHP);
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Clear();
                     }
                     else if (hitAccuracy > 8)
                     {
@@ -72,20 +74,24 @@ namespace ConsoleApplication1
                     //will hit dragon from 10-15 hp 
                     dragonHP = dragonHP - (rng.Next(10, 16));
                     Console.WriteLine("The dragon has {0} hp.", dragonHP);
-                    
+                    System.Threading.Thread.Sleep(800);
+                    Console.Clear();
                 }
                 else if (userChoice == "3")
                 {
+                    //will heal the player with a random amount between 10-20
                     playerHP = playerHP + (rng.Next(10, 21));
                     Console.WriteLine("The dragon has {0} hp", dragonHP);
-                    
+                    System.Threading.Thread.Sleep(800);
+                    Console.Clear();
                 }
                 else
                 {
                     //will display when any input rather than 1,2 or 3 is chosen
                     Console.WriteLine("Please enter a valid input, idiot.");
                     Console.WriteLine("You now have {0} hp", playerHP);
-                    
+                    System.Threading.Thread.Sleep(800);
+                    Console.Clear();
                 }
         }
         /// <summary>
@@ -94,20 +100,22 @@ namespace ConsoleApplication1
         static void DragonAttack()
         {
             int fireAcc = rng.Next(0, 11);
-            //only lets the dragon hit if 1-8 is randomly generated
-            if (fireAcc > 9)
-            {
 
-                if ("123".Contains(userChoice))
+            if ("123".Contains(userChoice))
+            {
+                //only lets the dragon hit if 1-8 is randomly generated
+                if (fireAcc < 9)
                 {
                     playerHP = playerHP - rng.Next(5, 16);
                     Console.WriteLine("You now have {0} hp", playerHP);
                 }
-            }
-            else
-            {
-                Console.WriteLine("The dragon missed. You may just live to see another day");
-                Console.WriteLine("You still have {0}hp", playerHP);
+                else
+                {
+                    Console.WriteLine("The dragon missed. You may just live to see another day");
+                    Console.WriteLine("You still have {0}hp", playerHP);
+                    System.Threading.Thread.Sleep(1100);
+                    Console.Clear();
+                }
             }
         }
         /// <summary>
